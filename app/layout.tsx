f1f0d1f841fe18530a8f42491f4930bcb67846d6
Aditya Singh <yeezusgod01@gmail.com>
2024-09-17 15:57:46 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Space_Grotesk({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Analytics></Analytics>
+        {children}
+      </body>
     </html>
   );
 }
