136ce9b0369796ddf5968cdb1710d2200afe96f5
Aditya Singh <yeezusgod01@gmail.com>
2024-09-17 22:01:09 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,12 @@ import { CardHoverEffectDemo } from "./card-hover-effect-snippet";
 import { CoverDemo } from "./sparkles-snippet";
 import FAQS from "./faq";
 import { FloatingNavDemo } from "./floating-navbar-snippet";
-import { ScrollBasedVelocityDemo } from "./scroll-based-velocity-snippet";
 
 export default function Home() {
   return (
     <div>
       <FloatingNavDemo></FloatingNavDemo>
       <BackgroundLinesDemo></BackgroundLinesDemo>
-      <section className="py-20">
-        <ScrollBasedVelocityDemo></ScrollBasedVelocityDemo>
-      </section>
       <TypewriterEffectSmoothDemo></TypewriterEffectSmoothDemo>
       <CoverDemo></CoverDemo>
       <CardHoverEffectDemo></CardHoverEffectDemo>
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
     images: {
         domains: [
             "github.blog",
+            "images.unsplash.com",
+            "assets.aceternity.com",
+            "cdn.dribbble.com",
+            "www.iowaclinic.com",
+            "cdn.myhealthteams.com",
+            "static.infragistics.com",
+            "i.pinimg.com",
+            "ui.aceternity.com",
         ],
     },
 };
