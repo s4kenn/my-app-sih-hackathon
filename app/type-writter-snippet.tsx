71c55897164509bfb378cc5d494c2af79851e5df
Aditya Singh <118333811+s4kenn@users.noreply.github.com>
2024-09-18 10:57:32 +0530

    Update type-writter-snippet.tsx

--- app/type-writter-snippet.tsx
+++ app/type-writter-snippet.tsx
@@ -27,10 +27,11 @@ export function TypewriterEffectSmoothDemo() {
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
         <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
           Go to ChatMedica
-        </button>
+        </button> <Link href="https://brain-tumor-chatmedica.streamlit.app/">
         <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
-      <Link href="https://brain-tumor-chatmedica.streamlit.app/">    Get your report </Link>
+         Get your report 
         </button>
+          </Link>
       </div>
     </div>
   );
